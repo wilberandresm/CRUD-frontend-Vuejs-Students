0641746ae8abf7c22147f796cbf9247e89b577d8
wilberandresm <wilberandresm@hotmail.com>
2019-12-09 09:09:35 -0500

    v1.1
    Table

--- src/components/StudentsComponent.vue
+++ src/components/StudentsComponent.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
+    <b-form >
+      <b-form-group
+        id="input-group-1"
+        label="Code:"
+        label-for="input-1"
+        description="We'll never share your code with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.code"
+ 
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+  
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
     <b-table striped hover :items="STUDENTS"></b-table>
   </div>
 </template>
@@ -12,6 +43,15 @@ import { TypesStore } from "./../store/types/store.types";
 import { mapActions, mapGetters } from "vuex";
 
 export default {
+  data(){
+    return{
+      form:{
+        code:'',
+        name:''
+
+      },
+    }
+  },
   methods: {
     ...mapActions({
       GET_STUDENTS: TypesStore.actions.GET_STUDENTS
